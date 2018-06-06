2523b469246b7484dfb222f9fc8284b85302f4a9
Mick Burgess <mburgess@reliaslearning.com>
2018-06-05 21:42:46 -0400

    add city array and on click event

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,34 @@
+// API keys
+// Ticketmaster = L33YkC9wH8KXB7RNBA4rbkakkaT9iKFP
+// Zomato = 3e2640fc9c2ecf3e452dfc86b352db7f
+
+var cityArray = [
+  {
+    state: "NC",
+    city: "Raleigh"
+  },
+
+  {
+    state: "NC",
+    city: "Chapel Hill"
+  },
+
+  {
+    state: "NC",
+    city: "Greensboro"
+  },
+
+  {
+    state: "NC",
+    city: "Wilmington"
+  },
+
+  {
+    state: "NC",
+    city: "Asheville"
+  }
+]
+
+$("submitButton").on("click", function() {
+
+});
