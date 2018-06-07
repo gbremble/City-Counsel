cfc255277a8314c26f9b47f2172cb5d60cbe9a2b
Mick Burgess <mburgess@reliaslearning.com>
2018-06-07 19:17:10 -0400

    test code

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,34 +1,54 @@
 // API keys
 // Ticketmaster = L33YkC9wH8KXB7RNBA4rbkakkaT9iKFP
 // Zomato = 3e2640fc9c2ecf3e452dfc86b352db7f
+$(document).ready(function() {
+  var cityArray = [
+    {
+      stateShort: "NC",
+      stateLong: "North Carolina",
+      city: "Raleigh"
+    },
 
-var cityArray = [
-  {
-    state: "NC",
-    city: "Raleigh"
-  },
+    {
+      stateShort: "NC",
+      stateLong: "North Carolina",
+      city: "Chapel Hill"
+    },
 
-  {
-    state: "NC",
-    city: "Chapel Hill"
-  },
+    {
+      stateShort: "NC",
+      stateLong: "North Carolina",
+      city: "Greensboro"
+    },
 
-  {
-    state: "NC",
-    city: "Greensboro"
-  },
+    {
+      stateShort: "NC",
+      stateLong: "North Carolina",
+      city: "Wilmington"
+    },
 
-  {
-    state: "NC",
-    city: "Wilmington"
-  },
+    {
+      stateShort: "NC",
+      stateLong: "North Carolina",
+      city: "Asheville"
+    }
+  ]
 
-  {
-    state: "NC",
-    city: "Asheville"
-  }
-]
+  $(document).on("click", "#searchButton", function(event) {
+    // prevent submit button from refreshing page
+    event.preventDefault();
+    
+    // grabbing and storing values from the inputs
+    var city = $("#inputCity").val().trim();
+    var state = $("#inputState").val();
 
-$("submitButton").on("click", function() {
-
+    function getRestaurants() {
+      var restaurantRow = $("<tr>");
+      console.log(restaurantRow);
+      var restaurantName = $("<td>").val("Taco Bell");
+      restaurantRow.append(restaurantName);
+      $("#restaurantTable").append(restaurantRow)
+    }
+    getRestaurants();
+  });
 });
