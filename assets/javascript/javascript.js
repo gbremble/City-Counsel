f8577e1ba06d2e89e09c1da361b94e2330279d5f
Mick Burgess <mburgess@reliaslearning.com>
2018-06-07 19:49:01 -0400

    add js link

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,31 +1,40 @@
 // API keys
 // Ticketmaster = L33YkC9wH8KXB7RNBA4rbkakkaT9iKFP
 // Zomato = 3e2640fc9c2ecf3e452dfc86b352db7f
-var cityName = ""
-var stateName = ""
-var countryName = ""
-var cityArray = [
-  {
-    state: "NC",
-    city: "Raleigh"
-  },
-
+$(document).ready(function() {
+  var cityName = ""
+  var stateName = ""
+  var countryName = ""
+  var cityArray = [
+    {
+      stateShort: "NC",
+      stateLong: "North Carolina",
+      city: "Raleigh"
+    }, 
+    {
+      stateShort: "NC",
+      stateLong: "North Carolina",
+      city: "Chapel Hill"
+    },
     {
       stateShort: "NC",
       stateLong: "North Carolina",
       city: "Greensboro"
     },
-
     {
       stateShort: "NC",
       stateLong: "North Carolina",
       city: "Wilmington"
     },
-
     {
       stateShort: "NC",
       stateLong: "North Carolina",
       city: "Asheville"
+    },
+    {
+      stateShort: "NC",
+      stateLong: "North Carolina",
+      city: "Durham"
     }
 ]
 
@@ -39,4 +48,4 @@ stateName = $("#inputState").val().trim();
 countryName = "USA"
 
 });
-
+});
