0c897b6246090e6ed80f91a6e926d906a1592f3a
Mitchell <MitchellBrooks@gmail.com>
2018-06-07 20:09:32 -0400

    refactoring

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -31,37 +31,4 @@ var cityArray = [
   }
 ]
 
-$("#searchButton").on("click", function() {
-  event.preventDefault();
-
-//declaring working variables
-console.log("hello")
-cityName = $("#inputCity").val().trim();
-stateName = $("#inputState").val().trim();
-countryName = "USA"
-
-//added since last PR
-// forecast URL vs weather URL http://api.openweathermap.org/data/2.5/forecast?q=Raleigh,NC,USA&apikey=28e4dca7e5a6a9336edc60ea02073b04
-//API key
-var APIKey = "28e4dca7e5a6a9336edc60ea02073b04";
-var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "," + stateName + "," + countryName + "&appid=" + APIKey;
-
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-
-      console.log(queryURL);
-      console.log(response);
-    var weatherDiv = $("<div>");
-    weatherDiv.text("test");
-    
-    $("body").append(weatherDiv)
-
-      // $("#weather-table > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDest + "</td><td>" + trainFreq + "</td><td>" + nextTrain + "</td><td>" + tilNextTrain + "</td></tr>");
-
-
-
-});
-});
 
