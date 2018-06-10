27911b803191bd1916ad6a92819856c2d2280413
Mitchell <MitchellBrooks@gmail.com>
2018-06-10 02:45:46 -0400

    working thru the api

---
+++ assets/javascript/events.js
@@ -0,0 +1,37 @@
+$("#searchButton").on("click", function(event) {
+  event.preventDefault();
+
+var  cityName = $("#inputCity").val().trim();
+var  stateName = $("#inputState").val().trim();
+var  countryName = "US"
+  
+// var newcityName = currentCity.name;
+// var newstateName = currentCity.stateShort;
+// var newcountryName = currentCity.country;
+// var newcityLat = currentCity.lat;
+// var newcityLon = currentCity.lon;
+
+for(var i = 0; i < cityArray.length; i++) {
+  console.log(cityArray[i].city);
+  if(cityName === cityArray[i].city) {
+    var cityLat = cityArray[i].lat;
+    var cityLon = cityArray[i].lon;
+    console.log("success")
+  }
+}
+console.log(cityLat);
+console.log(cityLon);
+
+var APIKey = "wmKcj9CwphKVnTWv";
+var queryURL = "https://api.eventful.com/rest/events/where=" + cityLat + "," + cityLon + "&within=25&authentication=" + APIKey;
+function getEvents(){
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      console.log(queryURL);
+      console.log(response);
+    });
+  }
+getEvents();
+})
