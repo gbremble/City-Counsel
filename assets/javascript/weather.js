6daf1731ecfa145126c572c73854f5b9f902e039
Mitchell <MitchellBrooks@gmail.com>
2018-06-11 16:56:10 -0400

    added comments throughout weather.js for ease of use

--- assets/javascript/weather.js
+++ assets/javascript/weather.js
@@ -1,11 +1,11 @@
 var cityName = "";
 var cityNum = 0;
 var currentCity = {};
-
+//search event
 $("#searchButton").on("click", function(event) {
   event.preventDefault();
   $("#weatherDiv").toggleClass("d-none", false);
-  // initialize table
+  // toggle the target div visibility and empty the results from last search
   $("#forecastRow").empty();
 
 
@@ -23,6 +23,7 @@ getCity();
 
 // var APIKey = "28e4dca7e5a6a9336edc60ea02073b04";
 var APIKey = "166a433c57516f51dfab1f7edaed8413";
+//api stuff
 var queryURL = "https://api.openweathermap.org/data/2.5/forecast/daily?q=" + currentCity.city + "," + currentCity.iso2 + "&cnt=6&appid=" + APIKey;
 var currentQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + currentCity.city + "," + currentCity.iso2 + "&appid=" + APIKey;
 function getForecast(){
@@ -33,16 +34,7 @@ function getForecast(){
       // console.log(queryURL);
       console.log(response);
 
-    // var weatherDiv = $("<div>");
-    // weatherDiv.addClass("card");
-    // weatherDiv.addClass("mt-3");
-    // weatherDiv.addClass("mb-3");
-    // weatherDiv.attr("id", "weatherDiv");
-    
-    // $("#resultsDiv").append(weatherDiv);
-
-    // $("#weatherDiv").html('    <div class="card-header bg-primary text-white">                <h3 class="card-title">Weather</h3>              </div>            <div class="card-body"> <div class="container"> <div class="row" id="forecastRow"><div class="col-sm-2 col-md-2 col-lg-2 col-xl-2" id="current">                          </div></div></div></div>');
-
+//for loop to get data from each day of the forecast
     for (i=0; i<5; i++) {
       var timeU = response.list[i].dt
       var tempLowK = response.list[i].temp.min
@@ -51,13 +43,14 @@ function getForecast(){
       var iconCode = response.list[i].weather[0].icon;
       var conditionDescription = response.list[i].weather[0].description;
       
-
+//converting data into usable pieces
       tempLow = Math.round((tempLowK * 1.8) - 459.67);
       tempHigh = Math.round((tempHighK * 1.8) - 459.67);
       time = moment.unix(timeU).format('ddd');
       iconURL = "https://openweathermap.org/img/w/" + iconCode + ".png";
       iconImg = '<img class="mt-0 mb-0 weatherIcon" src=' + iconURL + ' height="45" width="45" alt="' + conditionDescription + '">';  
 
+      //create a responsive column to hold each card (which we will create shortly)
       var dayDiv = $("<div>");
       dayDiv.addClass("text-justify");
       dayDiv.addClass("col-xs-12")
@@ -65,18 +58,21 @@ function getForecast(){
       dayDiv.addClass("col-md-4");
       dayDiv.addClass("col-lg-4");
       dayDiv.addClass("col-xl-2");
+      //create a card for each day of the forecast
       var weathDiv = $("<div>");
       weathDiv.addClass("card ml-0 mr-0 mt-2 mb-2 h-95")
       weathDiv.attr("dayNum", i+1);
+      //float the name and weather icon on opposite sides of the card header
       weathDiv.append("<div class='card-header bg-secondary text-white'><div class='float-left'><h3 class='align-bottom pb-0 mb-0'>" + time + "</h3></div><div class='float-right small mt-0 pt-0'>" + iconImg + "</div><div class='clearfix'></div></div>");
-      // weathDiv.append(iconImg + "<br>")
-      // weathDiv.append('<h4 class="pt-0 pb-0 mt-0 mb-0">' + time + '</h4><br>')
+      //create the card body to contain conditions and temperatures
       var weathBody = $("<div class='card-body mb-4 mt-4'>");
       weathBody.append('<h3 class ="card-title text-truncate align-top pt-0 pb-0 mt-0 mb-0">' + conditions + "</h3><br>")
       weathBody.append("<h5 class='card-subtitle'>High: " + tempHigh + "º</h5>");
       weathBody.append("<h5 class='card-subtitle'>Low: " + tempLow + "º </h5>");
       weathDiv.append(weathBody);
       dayDiv.append(weathDiv);
+
+      //adding our column>card>header+body to the div
       $("#forecastRow").append(dayDiv);
     }
 });};
@@ -89,7 +85,7 @@ function getCurrentWeather(){
   // console.log(currentQueryURL);
   // console.log(currentResponse);
 
-  // var timeU = response.dt
+    //converting data into usable pieces
       var tempK = currentResponse.main.temp;
       var conditions = currentResponse.weather[0].main;
       var iconCode = currentResponse.weather[0].icon;
@@ -97,10 +93,10 @@ function getCurrentWeather(){
 
       temp = Math.round((tempK * 1.8) - 459.67);
 
-      // time = moment.unix(timeU).format('dddd');
       iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
       iconImg = '<img class="mb-0" src=' + iconURL + ' height="45" width="45" alt=' + conditionDescription + '>';  
 
+      //creating responsive column for our current weather
       var dayDiv = $("<div>");
       dayDiv.addClass("text-justify");
       dayDiv.addClass("col-xs-12")
@@ -108,18 +104,23 @@ function getCurrentWeather(){
       dayDiv.addClass("col-md-4");
       dayDiv.addClass("col-lg-4");
       dayDiv.addClass("col-xl-2");
+      //creating a card for current weather
       var weathDiv = $("<div>");
       weathDiv.addClass("card ml-0 mr-0 mt-2 mb-2 h-95")
       weathDiv.attr("dayNum", i+1);
+      //adding a header with title and icon
       weathDiv.append("<div class='card-header bg-secondary text-white'><div class='float-left'><h3 class='align-bottom pb-0 mb-0'> Now </h3></div><div class='float-right small mt-0 pt-0'>" + iconImg + "</div><div class='clearfix'></div></div>");
+      //creating a card body to hold the conditions and temp
       var weathBody = $("<div class='card-body mb-4  mt-4'>");
       weathBody.append('<h3 class ="card-title text-truncate align-top pt-0 pb-200 mt-0 mb-0">' + conditions + "</h3><br>")
       weathBody.append("<h2 class='card-subtitle lh-2'>" + temp + "º</h2>");
+      //combining the ingredients
       weathDiv.append(weathBody);
       dayDiv.append(weathDiv);
-
+      //adding our column>card>header+body to the div, and prepending it so it shows up in front of our forecast
       $("#forecastRow").prepend(dayDiv);
 });};
+//calling our two functions, delaying the current forecast so that the columns are in place before it prepends
 getForecast();
 setTimeout(getCurrentWeather, 500);
 })
