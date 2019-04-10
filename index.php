2e9b04a0ff1903fef7542a3d4626cdda260073a2
rscottjohnson <sjohnson.wvu@gmail.com>
2019-04-10 11:29:59 -0400

    Update for Heroku deploy

---
+++ index.php
@@ -0,0 +1,3 @@
+<!-- This php file as well as the composer.json file are for the purposes of deploying to Heroku -->
+<!-- This change included renaming the index.html file to home.html -->
+<?php include_once("home.html"); ?>
